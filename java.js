1e6ffa15b89b7d756a5fee36ad21cab48be3baab
Amanda Mathias <104474884+Amanda20052009@users.noreply.github.com>
2023-08-28 14:47:05 -0400

    Delete java.js

--- java.js
+++
@@ -1,6 +0,0 @@
-clipPath.attr('d', path1).animate({'d': path2}, duration, firstCustomMinaAnimation, function(){
-   clipPath.animate({'d': path3}, duration, secondCustomMinaAnimation, function(){
-      oldSlide.removeClass('visible');
-      newSlide.addClass('visible').removeClass('is-animating');
-   });
-});
